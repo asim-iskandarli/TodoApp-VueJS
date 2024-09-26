1958c1074fcdc4b07474093981358feabb4db9a3
asim-iskandarli <asim.iskandarli@gmail.com>
2024-09-26 18:02:47 +0400

    update vide config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/TodoApp-VueJS/",
+  base: "https://asim-iskandarli.github.io/TodoApp-VueJS/",
   plugins: [
     vue(),
   ],
